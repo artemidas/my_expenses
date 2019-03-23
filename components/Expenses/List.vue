883dc273ec56e9939b774e784b43be6fa1ab0978
Artemidas <palladin.93@hotmail.com>
2019-03-23 16:26:50 +0100

    Get total

--- components/Expenses/List.vue
+++ components/Expenses/List.vue
@@ -1,16 +1,19 @@
 <template>
-  <ul>
-    <li v-for="expense in expenses" :key="expense.id">
-      <p>
-        {{expense.name}} - Fee {{expense.fee}} - Periodicity
-        {{expense.periodicity}}
-      </p>
-      <button @click="openExpenseEdit(expense)">Edit</button>
-      <ExpenseEditForm v-if="expense.id === editingExpense" :data="expense">
-        <button @click="closeExpenseEdit" type="button">Cancel</button>
-      </ExpenseEditForm>
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li v-for="expense in expenses" :key="expense.id">
+        <p>
+          {{expense.name}} - Fee {{expense.fee}} - Periodicity
+          {{expense.periodicity}}
+        </p>
+        <button @click="openExpenseEdit(expense)">Edit</button>
+        <ExpenseEditForm v-if="expense.id === editingExpense" :data="expense">
+          <button @click="closeExpenseEdit" type="button">Cancel</button>
+        </ExpenseEditForm>
+      </li>
+    </ul>
+    <p>Total {{ total }}</p>
+  </div>
 </template>
 
 <script>
@@ -22,10 +25,18 @@ export default {
   components: {
     ExpenseEditForm
   },
-  computed: mapGetters({
-    expenses: 'expenses',
-    editingExpense: 'editingExpense'
-  }),
+  computed: {
+    total() {
+      return this.expenses.reduce(
+        (accumulator, expense) => accumulator + Number(expense.fee),
+        0
+      )
+    },
+    ...mapGetters({
+      expenses: 'expenses',
+      editingExpense: 'editingExpense'
+    })
+  },
   methods: {
     openExpenseEdit(expense) {
       this.$store.dispatch('openExpenseEdit', expense)
